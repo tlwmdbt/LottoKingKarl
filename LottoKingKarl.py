7b2548cbfdc8f30e97215cea2cf5a11d607d2b25
Daniel Toschläger <d.toschlaeger@gmail.com>
2020-09-13 15:23:07 +0200

    Update LottoKingKarl.py

--- LottoKingKarl.py
+++ LottoKingKarl.py
@@ -10,23 +10,27 @@ from random import *
 # Banner
 print("\n###", "Lotto King Karl", "###")
 
-# Eine Liste
+# Empty list
 LottoZahlenListe = []
 
-# 6 Lottozahlen + Superzahl
+# 6 regular + 1 special number, all from within a range from 1 to 49
 while len(LottoZahlenListe) < 8:
   LottoZahl=randint(1,49)
 
-  # und die zufällig generierte Zahl nicht schon in der Liste steht
+  # check if number already in list
   if LottoZahl not in LottoZahlenListe:
-    # die gnerierte Zahl an die Liste anhängen
+    # if not already in list append to it
     LottoZahlenListe.append(LottoZahl)
 
-# Lotto Zahlen Liste ausgeben
-# Zähler Variable für die Ausgabe
+# Output generated numbers #########################################
+#
+# counter for display loop
 i = 1
 
-# Die ersten 6 Einträge in der Liste ausgeben
+# Print the first 6 entries of the list
 while i < 7:
   print("LottoZahl ", i, ": ", LottoZahlenListe[i])
   i = i + 1
+  
+  # print the SuperZahl
+  print("SuperZahl: ", LottoZahlenListe[7], "\n")
