6ba0dd9482a7d535e0437abf8b0a3d60650c5bc9
Daniel Toschläger <d.toschlaeger@gmail.com>
2020-09-13 15:49:49 +0200

    Update LottoKingKarl.py

--- LottoKingKarl.py
+++ LottoKingKarl.py
@@ -32,5 +32,5 @@ while i < 7:
   print("LottoZahl ", i, ": ", LottoZahlenListe[i])
   i = i + 1
   
-  # print the SuperZahl
-  print("SuperZahl: ", LottoZahlenListe[7], "\n")
+# print the SuperZahl
+print("SuperZahl: ", LottoZahlenListe[7], "\n")
