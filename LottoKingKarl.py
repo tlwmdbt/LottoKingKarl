7be497a57e526ac137e383405d3ab9174490fc81
Daniel Toschläger <d.toschlaeger@gmail.com>
2020-09-13 15:10:31 +0200

    Create LottoKingKarl.py
    Version 1

---
+++ LottoKingKarl.py
@@ -0,0 +1,30 @@
+# Lotto King Karl Zahlen Generator
+# Daniel Toschlaeger, 10.09.2020
+#
+#================================
+from math import *
+from random import *
+#================================
+# Banner
+print("\n###", "Lotto King Karl", "###")
+
+# Eine Liste
+LottoZahlenListe = []
+
+# 6 Lottozahlen + Superzahl
+while len(LottoZahlenListe) < 8:
+  LottoZahl=randint(1,49)
+
+  # und die zufällig generierte Zahl nicht schon in der Liste steht
+  if LottoZahl not in LottoZahlenListe:
+    # die gnerierte Zahl an die Liste anhängen
+    LottoZahlenListe.append(LottoZahl)
+
+# Lotto Zahlen Liste ausgeben
+# Zähler Variable für die Ausgabe
+i = 1
+
+# Die ersten 6 Einträge in der Liste ausgeben
+while i < 7:
+  print("LottoZahl ", i, ": ", LottoZahlenListe[i])
+  i = i + 1
